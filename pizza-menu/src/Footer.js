5d400ada014e9c053b55ba3190ec2d4c98768718
SrestMilan <shrestham780@gmail.com>
2023-09-27 16:43:11 +0545

    styling component

--- pizza-menu/src/Footer.js
+++ pizza-menu/src/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  //   else alert("Sorry we're close");
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
   );
--- pizza-menu/src/Header.js
+++ pizza-menu/src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  return (
+    <h1 style={{ color: "red", fontSize: "48px", textTransform: "uppercase" }}>
+      Fast React Pizza Co.
+    </h1>
+  );
 }
 
 export default Header;
